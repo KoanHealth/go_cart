ed35ad6bf56580836169c512f161e77493453d49
Brad Smalling <brad.smalling@koanhealth.com>
2012-12-04 22:33:02 -0600

    Added a little guessing to the go_cart sql command

--- bin/command/go_cart_sql.rb
+++ bin/command/go_cart_sql.rb
@@ -15,9 +15,125 @@ class GoCartSql < GoCartDef
 		from_table = from_schema.get_table(@from_table.to_sym)
 		to_table = to_schema.get_table(@to_table.to_sym)
 
+		mapping = map_fields(from_table, to_table)
 		template_directory = File.join(@script_dir, '../../templates')
 		template = IO.read(File.join(template_directory, 'insert_select.sql.erb'))
 		puts ERB.new(template).result(binding)
+	end
+
+	def map_fields(from_table, to_table)
+		mapping = {}
+
+		from_words = {}
+		from_table.fields.each do |symbol, field|
+			from_words[symbol] = split_into_words(symbol.to_s)
+		end
+
+		to_words = {}
+		to_table.fields.each do |symbol, field|
+			to_words[symbol] = split_into_words(symbol.to_s)
+		end
+
+		to_words.each do |to_symbol, to_list|
+			mapping[to_symbol] = nil
+			from_words.each do |from_symbol, from_list|
+				hits = misses = 0
+				to_list.each do |to_word|
+					if from_list.include?(to_word)
+						hits += 1
+					else
+						misses += 1
+					end
+				end
+				extra = from_list.count - hits
+
+				if misses >= 0 && (hits > 1 || extra < 1)
+					mapping[to_symbol] = from_symbol
+					next
+				end
+			end
+		end
+
+		return mapping
+	end
+
+	def split_into_words(name)
+		name = name.gsub(/(.+)(\d+)/, '\1_\2')
+		parts = name.split(/_+/)
+		parts.reject! { |word| reject_word(word) }
+		parts.map! { |word| replace_word(word) }
+		return parts
+	end
+
+	@@rejects = {
+			'and' => true,
+			'for' => true,
+			'of' => true,
+			'on' => true,
+			'or' => true,
+	}
+
+	def reject_word(word)
+		return @@rejects[word] || false
+	end
+
+	@@replacements = {
+	    'alt' => 'alternate',
+	    'amt' => 'amount',
+	    'beg' => 'begin',
+	    'cd' => 'code',
+	    'desc' => 'description',
+	    'descr' => 'description',
+	    'dep' => 'dependent',
+	    'diag' => 'diagnosis',
+	    'div' => 'division',
+	    'dob' => 'birthdate',
+	    'dt' => 'date',
+	    'dx' => 'diagnosis',
+	    'ee' => 'employee',
+	    'eff' => 'effective',
+	    'flg' => 'flag',
+	    'grp' => 'group',
+	    'id' => 'identifier',
+	    'ln' => 'line',
+	    'loc' => 'location',
+	    'mbr' => 'member',
+	    'med' => 'medical',
+	    'mi' => 'middle',
+	    'mid' => 'middle',
+	    'mod' => 'modifier',
+	    'nbr' => 'number',
+	    'nm' => 'name',
+	    'num' => 'number',
+	    'org' => 'organization',
+	    'orig' => 'original',
+	    'pd' => 'paid',
+	    'phys' => 'physician',
+	    'pri' => 'primary',
+	    'proc' => 'procedure',
+	    'prov' => 'provider',
+	    'prv' => 'provider',
+	    'rel' => 'relationship',
+	    'rev' => 'revenue',
+	    'rx' => 'pharmacy',
+	    'sec' => 'secondary',
+	    'seq' => 'sequence',
+	    'serv' => 'service',
+	    'sex' => 'gender',
+	    'spec' => 'specialty',
+	    'st' => 'state',
+	    'svc' => 'service',
+	    'term' => 'termination',
+	    'thru' => 'through',
+	    'tot' => 'total',
+	    'typ' => 'type',
+	    'un' => 'units',
+	    'val' => 'value',
+	    'ver' => 'version',
+	}
+
+	def replace_word(word)
+		return @@replacements[word] || word
 	end
 
 	def parse_options(opts)
