f0c8240e32898a7adaa570d28dcaecb522f82adf
Jerry Clinesmith <jerryclinesmith@gmail.com>
2013-02-16 07:05:22 -0600

    Script to rebuild/reinstall gem

---
+++ script/reinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$SCRIPT_DIR/../"
+pushd $PROJECT_DIR
+
+gem build go_cart.gemspec
+for f in *.gem; do gem install $f; done
+rm -rf *.gem
+
+popd
